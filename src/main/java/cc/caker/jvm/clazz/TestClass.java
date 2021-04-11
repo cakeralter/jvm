05cdeabf3ddcf1bb3645640a1e77d9afa671d940
cakeralter <fei519881991@gmail.com>
2021-04-11 11:59:01 +0800

    demo: Class文件结构

---
+++ src/main/java/cc/caker/jvm/clazz/TestClass.java
@@ -0,0 +1,15 @@
+package cc.caker.jvm.clazz;
+
+/**
+ * @author cakeralter
+ * @date 2021/4/11
+ * @since 1.0
+ */
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
