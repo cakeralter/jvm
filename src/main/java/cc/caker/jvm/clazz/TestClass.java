202a008effcab351b93222ad66a5496ab7301f96
cakeralter <fei519881991@gmail.com>
2021-04-11 18:15:01 +0800

    demo: 方法表集合&字段表集合

--- src/main/java/cc/caker/jvm/clazz/TestClass.java
+++ src/main/java/cc/caker/jvm/clazz/TestClass.java
@@ -1,5 +1,8 @@
 package cc.caker.jvm.clazz;
 
+import java.security.InvalidParameterException;
+import java.time.LocalDateTime;
+
 /**
  * @author cakeralter
  * @date 2021/4/11
@@ -11,5 +14,13 @@ public class TestClass {
 
     public int inc() {
         return m + 1;
+    }
+
+    public String testSignature(String name, LocalDateTime time, int sort) throws InvalidParameterException {
+        if (name == null || name.length() == 0) {
+            throw new InvalidParameterException();
+        }
+        System.out.printf("[%d]%s signature at %s%n", sort, name, time);
+        return "ok";
     }
 }
